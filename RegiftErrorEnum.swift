2df4c7b9b29cb14f01524d2624e6079ee11de469
Shir Bar Lev <shirblc@gmail.com>
2022-05-02 12:09:46 +0300

    Added Objective-C compatible version of the RegiftError enum

--- RegiftErrorEnum.swift
+++ RegiftErrorEnum.swift
@@ -9,8 +9,31 @@
 import Foundation
 
 // Errors thrown by Regift
-public enum RegiftError: String, ErrorType {
+public enum RegiftError: String, Error {
     case DestinationNotFound = "The temp file destination could not be created or found"
     case AddFrameToDestination = "An error occurred when adding a frame to the destination"
     case DestinationFinalize = "An error occurred when finalizing the destination"
 }
+
+// Objective-C compatible version
+@objc enum RegiftErrorObjC: Int, Error {
+    case DestinationNotFound = 1
+    case AddFrameToDestination
+    case DestinationFinalize
+}
+
+extension RegiftErrorObjC {
+    var localizedDescription: String {
+        switch(self) {
+        case .DestinationNotFound:
+            return "The temp file destination could not be created or found"
+        case .AddFrameToDestination:
+            return "An error occurred when adding a frame to the destination"
+        case .DestinationFinalize:
+            return "An error occurred when finalizing the destination"
+        @unknown default:
+            return "An unknown error occurred"
+        }
+    }
+}
+
